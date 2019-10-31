baad221c9b15a901b13039879e9447920be05a6f
Margotte <margo.tinawi@gmail.com>
2019-10-31 16:48:46 +0100

    Halloween front end

--- app/javascript/component/navbar.js
+++ app/javascript/component/navbar.js
@@ -3,9 +3,9 @@ const initUpdateNavbarOnScroll = ()=> {
 
   window.addEventListener("scroll", () => {
     if (window.scrollY >= window.innerHeight) {
-      navbar.classList.add('navbar-lewagon-white');
+      navbar.classList.add('navbar-lewagon-black');
     } else {
-      navbar.classList.remove('navbar-lewagon-white');
+      navbar.classList.remove('navbar-lewagon-black');
     }
   });
 }
